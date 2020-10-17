32dc6ac037ac32bcc75b5aab6268b857cd4d6ce8
edson.junior <edson.junior@sinqia.com.br>
2020-10-17 17:00:18 -0300

    Commit init estrutura inicial

---
+++ DesafioPagCerto.Core/Controllers/Transactions/TransactionController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DesafioPagCerto.Controllers.Transactions
+{
+    [ApiController]
+    [Route("transaction/v1")]
+    public class TransactionController : ControllerBase
+    {
+        
+    }
+}
---
+++ DesafioPagCerto.Entities/Transaction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DesafioPagCerto.Entities
+{
+    public class Transaction
+    {
+        private int NSU { get; }
+        private DateTime DateTransaction { get;}
+        private DateTime DateApproved { get;}
+        private DateTime DateReapproved { get;}
+        private bool Anticipation { get;}
+        private bool Confirmation { get;}
+        private decimal GrossValue { get;}
+        private decimal NetValue { get;}
+        private decimal TaxFixed { get;}
+        private int NumberParcel { get;}
+        private string CredCard { get;}
+    }
+}
