cdf43eabc0f53da106fc0730d3218c5614055aa4
edson.junior <edson.junior@sinqia.com.br>
2020-10-20 14:21:04 -0300

    Refatorando os testes

--- DesafioPagCerto.Tests/TransactionServiceTest.cs
+++ DesafioPagCerto.Tests/TransactionServiceTest.cs
@@ -19,90 +19,65 @@ namespace DesafioPagCerto.Tests
         public void TestTransactionTaxOK()
         {
             var tra = _transactionService.CreateTransaction("5909999999999999", 2, 1000);
-            if (tra.Confirmation && tra.NetValue == tra.GrossValue - tra.FixedTax)
-                Assert.Pass("Ok");
-            else
-                Assert.Fail("Error");
+            Assert.True(tra.Confirmation);
+            Assert.AreEqual(0.90, tra.GrossValue - tra.NetValue);
         }
 
         [Test]
         public void TestTransactionTaxNotOK()
         {
             var tra = _transactionService.CreateTransaction("5999999999999999", 2, 1000);
-            if (!tra.Confirmation && tra.NetValue == tra.GrossValue)
-                Assert.Pass("Ok");
-            else
-                Assert.Fail("Error");
+            Assert.False(tra.Confirmation);
+            Assert.AreEqual(tra.GrossValue, tra.NetValue);
         }
 
         [Test]
         public void TestCreateTransactionNumberCardOK()
         {
             var tra = _transactionService.CreateTransaction("5909999999999999", 2, 1000);
-            if (tra.Confirmation)
-                Assert.Pass("Ok");
-            else
-                Assert.Fail("Error");
+            Assert.True(tra.Confirmation);
         }
 
         [Test]
         public void TestCreateTransactionNumberCardNotOK()
         {
             var tra = _transactionService.CreateTransaction("5999999999999999", 2, 1000);
-            if (!tra.Confirmation)
-                Assert.Pass("Ok");
-            else
-                Assert.Fail("Error");
+            Assert.False(tra.Confirmation);
         }
 
         [Test]
         public void TestCreateTransactionNumberCardLenghtOK()
         {
             var tra = _transactionService.CreateTransaction("5909999999999999", 2, 1000);
-            if (tra.Confirmation)
-                Assert.Pass("Ok");
-            else
-                Assert.Fail("Error");
+            Assert.True(tra.Confirmation);
         }
 
         [Test]
-        public void TestCreateTransactionNumberCardLenghtMinorOK()
+        public void TestCreateTransactionNumberCardLenghtMinorNotOK()
         {
             var tra = _transactionService.CreateTransaction("59099999999999", 2, 1000);
-            if (!tra.Confirmation)
-                Assert.Pass("Ok");
-            else
-                Assert.Fail("Error");
+            Assert.False(tra.Confirmation);
         }
 
         [Test]
-        public void TestCreateTransactionNumberCardLenghtLargerOK()
+        public void TestCreateTransactionNumberCardLenghtLargerNotOK()
         {
             var tra = _transactionService.CreateTransaction("59099999999997999", 2, 1000);
-            if (!tra.Confirmation)
-                Assert.Pass("Ok");
-            else
-                Assert.Fail("Error");
+            Assert.False(tra.Confirmation);
         }
 
         [Test]
         public void TestCreateTransactionConfirmationOK()
         {
             var tra = _transactionService.CreateTransaction("5909999999999999", 2, 1000);
-            if (tra.Confirmation)
-                Assert.Pass("Ok");
-            else
-                Assert.Fail("Error");
+            Assert.True(tra.Confirmation);
         }
 
         [Test]
         public void TestCreateTransactionConfirmationNotOK()
         {
             var tra = _transactionService.CreateTransaction("5999999999999999", 2, 1000);
-            if (!tra.Confirmation)
-                Assert.Pass("Ok");
-            else
-                Assert.Fail("Error");
+            Assert.False(tra.Confirmation);
         }
     }
 }
