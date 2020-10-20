1f4e5efa15e708bfeb2f42de2303ebe96273b0a8
edson.junior <edson.junior@sinqia.com.br>
2020-10-20 20:03:28 -0300

    Estrutura de Teste para Antecipação

---
+++ DesafioPagCerto.Tests/AnticipationServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace DesafioPagCerto.Tests
+{
+    public class AnticipationServiceTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestCreateAnticipationNotOk()
+        {
+            
+        }
+    }
+}
